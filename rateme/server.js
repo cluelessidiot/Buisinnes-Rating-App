6b7122726e29fdcea4a84187b99aba19d0e350f4
AATHIL.TA <33000688+cluelessidiot@users.noreply.github.com>
2019-05-12 01:23:36 +0530

    updated

--- rateme/server.js
+++ rateme/server.js
@@ -4,7 +4,17 @@ var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var engine = require('ejs-mate');
 var session = require ('express-session');
+
+
 var app = express();
+
+app.engine('ejs',engine);
+app.set('view engine','ejs');
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+
+
 app.listen(3000, function(){
     console.log('Listening to Port 3000');
 }) 
