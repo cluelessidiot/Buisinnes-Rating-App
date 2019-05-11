96d1b69a8c1f8e7cdef782895e5c9843aae92361
AATHIL.TA <33000688+cluelessidiot@users.noreply.github.com>
2019-05-12 02:04:18 +0530

    sample test server

--- rateme/server.js
+++ rateme/server.js
@@ -7,14 +7,18 @@ var session = require ('express-session');
 
 
 var app = express();
-
+//we declare public folder as static ,so we have acces to allfolder inide it
+app.use(express.static('public'));
 app.engine('ejs',engine);
+//view engine
 app.set('view engine','ejs');
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 
-
+app.get('/',function(req,res,next){
+    res.render('index');
+})
 app.listen(3000, function(){
     console.log('Listening to Port 3000');
 }) 
