92fa4b285bb36894e9eb4871ab357dcbcf1a84d7
AATHIL.TA <33000688+cluelessidiot@users.noreply.github.com>
2019-05-12 00:42:57 +0530

    updated ejs,expression-session,ejs mate requirement

--- rateme/server.js
+++ rateme/server.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
+var session = require ('express-session');
 var app = express();
 app.listen(3000, function(){
     console.log('Listening to Port 3000');
-})
+}) 
