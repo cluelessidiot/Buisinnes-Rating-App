aebc8533f99edc8fba77b98a1d6bc02de043217c
AATHIL.TA <33000688+cluelessidiot@users.noreply.github.com>
2019-05-13 00:17:08 +0530

    user updated

--- rateme/routes/user.js
+++ rateme/routes/user.js
@@ -1,9 +1,9 @@
 module.exports = (app) => {
     
     app.get('/',(req,res,next) =>{
-    res.render('index',{title: rateme || df});
+    res.render('index');
 });
    app.get('/signup',(req,res) =>
-          {res.render('user/signup',{title: signup} );
+          {res.render('user/signup');
           });
 }
