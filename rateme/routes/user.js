151fede20201d05722783d8634f9922eb2583daa
AATHIL.TA <33000688+cluelessidiot@users.noreply.github.com>
2019-05-12 17:18:34 +0530

    updating user.js

--- rateme/routes/user.js
+++ rateme/routes/user.js
@@ -1,7 +1,9 @@
 module.exports = (app) => {
     
-    app.get('/',function(req,res,next){
-    res.render('index');
-})
-
+    app.get('/',(req,res,next) =>{
+    res.render('index',{title: rateme || df});
+});
+   app.get('/signup',(req,res) =>
+          {res.render('user/signup',{title: signup} );
+          });
 }
